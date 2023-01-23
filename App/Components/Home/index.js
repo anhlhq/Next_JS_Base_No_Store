2e5298c8d978c075538fd99caa61c81533fa1d83
Le Huy Quang Anh (K.NHS-HO) <anhlhq@pvcombank.com.vn>
2023-01-23 14:14:51 +0700

    update

--- App/Components/Home/index.js
+++ App/Components/Home/index.js
@@ -2,12 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import { Layout } from 'Components/Commons'
 const Home = ({ title }) => {
-  return (
-    <Layout title={title}>
-      Test
-      <Link href="/about">Push</Link>
-    </Layout>
-  )
+  return <Layout title={title}>Hello</Layout>
 }
 
 export default Home
